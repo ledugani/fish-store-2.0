d0267822efbe2628e8a1121fa58f93dbed510500
Tom Dugan <thomas@aadwebb.com>
2018-07-07 11:31:33 -0500

    created public route

--- src/app/App.js
+++ src/app/App.js
@@ -11,7 +11,7 @@ import Inventory from '../components/Inventory/Inventory';
 // import New from '../components/New/New';
 // import Order from '../components/Order/Order';
 // import OrderSpa from '../components/OrderSpa/OrderSpa';
-// import Register from '../components/Register/Register';
+import Register from '../components/Register/Register';
 // import SingleOrder from '../components/SingleOrder/SingleOrder';
 
 const PrivateRoute = ({ component: Component, authed, ...rest}) => {
@@ -24,6 +24,23 @@ const PrivateRoute = ({ component: Component, authed, ...rest}) => {
         ) : (
           <Redirect
             to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === false ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/orders', state: {from: props.location}}}
           />
         )
       }
@@ -50,6 +67,11 @@ class App extends Component {
                     path="/inventory"
                     authed={this.state.authed}
                     component={Inventory}
+                  />
+                  <PublicRoute
+                    path="/register"
+                    authed={this.state.authed}
+                    component={Register}
                   />
                 </Switch>
               </div>
