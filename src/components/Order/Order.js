aa49972e9e97c4f1ab0c7dd689d608f5055a8b5d
Tom Dugan <thomas@aadwebb.com>
2018-07-10 19:02:36 -0500

    adds prices to order total

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Order.css';
+import formatPrice from '../../helpers';
 
 class Order extends React.Component {
   renderOrder = (key) => {
@@ -8,19 +9,37 @@ class Order extends React.Component {
     const count = this.props.order[key];
 
     return (
-      <li>
-        {count} lbs. {fish.name}
+      <li
+        key={key}
+        className="text-left"
+      >
+        <div className="col-xs-2 count">{count} lbs.</div>
+        <div className="col-xs-5">{fish.name}</div>
+        <div className="col-xs-3">{formatPrice(fish.price)}</div>
+        <div className="col-xs-2">
+          <button className="btn btn-default">&times;</button>
+        </div>
       </li>
     );
   }
 
   render () {
     const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes.find(x => x.id === key)
+      const count = this.props.order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+
     return (
       <div className="Order">
         <h1>Order</h1>
         {orderIds.map(this.renderOrder)}
-        <div className="total">Total</div>
+        <div className="total">Total: <strong>{formatPrice(total)}</strong></div>
         <button className="btn btn-default">Save Order</button>
       </div>
     );
